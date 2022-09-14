ab4d7438c64d233bfb8a1cd9e2261caa6899e9f6
KoVal177 <58688524+KoVal177@users.noreply.github.com>
2022-09-14 12:08:17 +0300

    Update codewars.scala

--- codewars.scala
+++ codewars.scala
@@ -14,3 +14,36 @@ object SheepAdvisor {
   }
 }
 
+object Sol {
+
+  def evenOrOdd(number: Int): String =
+    number % 2 match
+      case 0 => "Even"
+      case _ => "Odd"
+}
+
+def maskify(cc: String): String =
+  if cc.length <= 4 then cc
+  else "#" + maskify(cc.tail)
+
+object MultiplesOf3Or5 {   
+  def solution(number: Int): Long =
+    def iter(threes: Int, fives: Int, s: Long, n: Int): Long =
+      if threes + 3 >= n && fives + 5 >= n then s
+      else if threes + 3 == fives + 5 then iter(threes + 3, fives + 5, s + threes + 3, n)
+      else if threes + 3 < fives + 5 then iter(threes + 3, fives, s + threes + 3, n)
+      else iter(threes, fives + 5, s + fives + 5, n)
+    iter(0, 0, 0, number)
+}
+
+object WhichAreIn {
+
+  def inArray(array1: Array[String], array2: Array[String]): Array[String] =
+    def iter(a1: Array[String], a_res: Array[String], a_condition: String): Array[String] =
+      if a1.isEmpty then a_res.sortWith(_ < _)
+      else if (a_condition contains a1.head) && !(a_res contains a1.head) then 
+        iter(a1.tail, a_res :+ a1.head, a_condition)
+      else
+        iter(a1.tail, a_res, a_condition)
+    iter(array1, Array(), array2.mkString(" "))
+}
